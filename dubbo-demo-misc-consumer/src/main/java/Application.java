00af4ac411826fd51b008736ddd20cb66d4b1d0e
wxx123 <stellarwxx@163.com>
2021-01-03 18:43:02 +0800

    add api context demo

--- dubbo-demo-misc-consumer/src/main/java/Application.java
+++ dubbo-demo-misc-consumer/src/main/java/Application.java
@@ -16,18 +16,20 @@
  */
 package org.apache.dubbo.demo.misc.consumer;
 
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.apache.dubbo.config.utils.ReferenceConfigCache;
 import org.apache.dubbo.demo.misc.DemoService;
 
+import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.concurrent.CompletableFuture;
 
 public class Application {
-    /**
-     * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
-     * launch the application
-     */
-    public static void main(String[] args) throws Exception {
+    public static void startWithXmlContext() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         context.start();
         DemoService demoService = context.getBean("demoService", DemoService.class);
@@ -35,5 +37,59 @@ public class Application {
         // System.out.println("result: " + hello.get());
         String hello = demoService.sayHello("world");
         System.out.println("result: " + hello);
+    }
+
+    private static boolean isClassic(String[] args) {
+        return args.length > 0 && "classic".equalsIgnoreCase(args[0]);
+    }
+
+    private static void runWithBootstrap() {
+        System.out.println("dubbo comsumer starting with bootstrap");
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+        reference.setInterface(DemoService.class);
+        reference.setGeneric("true");
+
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        bootstrap.application(new ApplicationConfig("demo-consumer"))
+                .registry(new RegistryConfig("zookeeper://192.168.2.101:2181"))
+                .reference(reference)
+                .start();
+
+        DemoService demoService = ReferenceConfigCache.getCache().get(reference);
+        String message = demoService.sayHello("dubbo");
+        System.out.println(message);
+
+        // generic invoke
+        GenericService genericService = (GenericService) demoService;
+        Object genericInvokeResult = genericService.$invoke("sayHello", new String[] { String.class.getName() },
+                new Object[] { "dubbo generic invoke" });
+        System.out.println(genericInvokeResult);
+    }
+
+    private static void runWithRefer() {
+        System.out.println("dubbo comsumer starting with refer");
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+        reference.setApplication(new ApplicationConfig("demo-consumer"));
+        reference.setRegistry(new RegistryConfig("zookeeper://192.168.2.101:2181"));
+        reference.setInterface(DemoService.class);
+        DemoService service = reference.get();
+        String message = service.sayHello("dubbo");
+        System.out.println(message);
+    }
+
+    public static void startWithApiContext(String[] args) {
+        if (isClassic(args)) {
+            runWithRefer();
+        } else {
+            runWithBootstrap();
+        }
+    }
+    /**
+     * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
+     * launch the application
+     */
+    public static void main(String[] args) throws Exception {
+        startWithXmlContext();
+        // startWithApiContext(args);
     }
 }
--- dubbo-demo-misc-provider/src/main/java/Application.java
+++ dubbo-demo-misc-provider/src/main/java/Application.java
@@ -16,13 +16,64 @@
  */
 package org.apache.dubbo.demo.misc.provider;
 
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.dubbo.demo.misc.DemoService;
+
 public class Application {
-    public static void main(String[] args) throws Exception {
+    public static void startWithXmlContext() throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-provider.xml");
         context.start();
         System.in.read();
+    }
+
+    private static boolean isClassic(String[] args) {
+        return args.length > 0 && "classic".equalsIgnoreCase(args[0]);
+    }
+
+    private static void startWithBootstrap() {
+        System.out.println("dubbo service starting with bootstrap");
+        ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
+        service.setInterface(DemoService.class);
+        service.setRef(new DemoServiceImpl());
+        service.setProtocol(new ProtocolConfig("dubbo", 20881));
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        bootstrap.application(new ApplicationConfig("demo-provider"))
+                .registry(new RegistryConfig("zookeeper://192.168.2.101:2181"))
+                .service(service)
+                .start()
+                .await();
+    }
+
+    private static void startWithExport() throws InterruptedException {
+        System.out.println("dubbo service starting with export");
+        ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
+        service.setInterface(DemoService.class);
+        service.setRef(new DemoServiceImpl());
+        service.setApplication(new ApplicationConfig("demo-provider"));
+        service.setRegistry(new RegistryConfig("zookeeper://192.168.2.101:2181"));
+        service.setProtocol(new ProtocolConfig("dubbo", 20881));
+        service.export();
+
+        new CountDownLatch(1).await();
+    }
+    public static void starWithApiContext(String[] args) throws InterruptedException {
+        if (isClassic(args)) {
+            startWithExport();
+        } else {
+            startWithBootstrap();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        startWithXmlContext();
+        // starWithApiContext(args);
     }
 }
 
